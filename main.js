de491e7b456af334477b088301143c554f1ef65e
Merry  HA <69305320+MerryHa@users.noreply.github.com>
2020-11-01 11:18:45 +0900

    JS: Project filtering

--- main.js
+++ main.js
@@ -52,6 +52,7 @@ arrowUp.addEventListener("click",()=>{
     scrollIntoView('#home');
 });
 
+
 //When mouse on contact me btn -> recover opacity
 homeContactBtn.addEventListener("mouseenter",()=>{
     homeContactBtn.style.opacity=1;
@@ -61,8 +62,29 @@ homeContactBtn.addEventListener('mouseleave', ()=>{
     homeContactBtn.style.opacity=1-(window.scrollY/homeHeight)+0.3;
 });
 
+//Project
+const workBtnContainer=document.querySelector('.work__categories');
+const projectContainer=document.querySelector('.work__projects');
+const projects=document.querySelectorAll('.project');
+workBtnContainer.addEventListener('click',(e)=>{
+const filter=e.target.dataset.filter||e.target.parentNode.dataset.filter;
+if(filter==null){
+    return;
+}
+projectContainer.classList.add('anim-out');
+setTimeout(()=>{
+    projects.forEach((project)=>{
+    if(filter==='*'||filter===project.dataset.type){
+        project.classList.remove('invisible');
+       
+    }else{
+        project.classList.add('invisible');
+    }
+});
+    projectContainer.classList.remove('anim-out');
+},300);
 
-
+});
 
 function scrollIntoView(selector){
     const scrollTo=document.querySelector(selector);
